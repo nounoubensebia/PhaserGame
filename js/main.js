78a1d28af586d3b2e1320b68d710321aa693c38b
Bill_Dev <db_thabet@esi.dz>
2018-06-03 04:45:31 +0200

    update thrw.png & main

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var game = new Phaser.Game(1000, 600);
-var dice, dices =[], pawn;
+var dice, player1Dices =[],player2Dices =[], pawn;
 var gameSceneW= 450, gameSceneH=600;
 var gameSceneOriginX = (1000-gameSceneW)/2 , gameSceneOriginY=50;
 
@@ -31,17 +31,25 @@ var mainState = {
 
     preload: function() {
         game.stage.backgroundColor = '#71c5cf';
-        game.load.spritesheet('dices-red', 'assets/Dices-red.png', 50, 50, 6);
+        game.load.spritesheet('dices-red', 'assets/Dices-Red.png', 50, 50, 6);
         game.load.spritesheet('dices-blue', 'assets/Dices-blue.png', 50, 50, 6);
         game.load.spritesheet('dices-green', 'assets/Dices-green.png', 50, 50, 6);
         game.load.spritesheet('dices-gray', 'assets/Dices-gray.png', 50, 50, 6);
         game.load.spritesheet('dices-yellow', 'assets/Dices-yellow.png', 50, 50, 6);
 
         game.load.image('bird', 'assets/bird.png');
-        game.load.image('pipe', 'assets/pipe.png');
-        game.load.image('pawn', 'assets/pawn.png');
+
+        game.load.image('throw', 'assets/throw.png');
+
+
+        // load pawns
+        game.load.image('pawn-gray', 'assets/pawn-gray.png');
+        game.load.image('pawn-green', 'assets/pawn-green.png');
+        game.load.image('pawn-yellow', 'assets/pawn-yellow.png');
         game.load.image('pawn-blue', 'assets/pawn-blue.png');
         game.load.image('pawn-red', 'assets/pawn-red.png');
+
+
 
 
     },
@@ -57,12 +65,21 @@ var mainState = {
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
 
-        this.score = 0;
-        this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
+
+        this.labelPlayer1 = game.add.text(20, 10, "Player 1", { font: "30px Arial", fill: "#ffffff" });
+        this.labelPlayer2 = game.add.text(20, 550, "Player 2", { font: "30px Arial", fill: "#ffffff" });
 
         this.gridUpdate();
         this.initializeDices();
-        //this.initializePawns();
+
+
+        var throwSprit1 = game.add.sprite(750, 560, 'throw');
+        throwSprit1.inputEnabled = true;
+        throwSprit1.events.onInputDown.add(this.throwPlayer1Dices, this);
+        var throwSprit2 = game.add.sprite(750, 10, 'throw');
+        throwSprit2.inputEnabled = true;
+        throwSprit2.events.onInputDown.add(this.throwPlayer2Dices, this);
+
         },
 
 
@@ -77,7 +94,7 @@ var mainState = {
         game.state.start('main');
     },
 
-    // generate a dice con click
+    // generate a dice on click
     generateDice: function(sprite){
         var rand = Math.floor(Math.random() * 6);
         sprite.frame = rand;
@@ -122,36 +139,39 @@ var mainState = {
                 switch(grille.matrix[index][1]) {
 
 
+
+                        //index 2: pawn id,  0= empty,  1=red, 2=yellow, 3=black, 4=green, 5=bleu * -1 = P2
+
                     case 1:
                         this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn-red');
                         break;
                     case 2:
-                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn');
+                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn-yellow');
                         break;
                     case 3:
-                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn');
+                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn-gray');
                         break;
                     case 4:
-                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn');
+                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn-green');
                         break;
                     case 5:
                         this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn-blue');
                         break;
 
                     case -1:
-                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn');
+                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn-red');
                         break;
                     case -2:
-                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn');
+                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn-yellow');
                         break;
                     case -3:
-                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn');
+                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn-gray');
                         break;
                     case -4:
-                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn');
+                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn-green');
                         break;
                     case -5:
-                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn');
+                        this.add.image(gameSceneOriginX + c*50, r*50+gameSceneOriginY, 'pawn-blue');
                         break;
                 }
 
@@ -180,63 +200,92 @@ var mainState = {
     initializeDices: function(){
 
     // itinialize player2's dices
-
-        var rand1 = Math.floor(Math.random() * 6);
-        dice = game.add.sprite(gameSceneOriginX+i*50, 0, 'dices');
-        dice.frame=rand;
+ var rand =0;
+        //1=red, 2=yellow, 3=black, 4=green, 5=bleu * -1 = P2
+        var dice =[];
+        dice[0] = game.add.sprite(gameSceneOriginX, 0, 'dices-red');
+        dice[0].frame=rand;
         //  Enables all kind of input actions on this image (click, etc)
-        dice.inputEnabled = true;
-        dice.events.onInputDown.add(this.generateDice, this);
-        dices.push(dice);
+        //dice[0].inputEnabled = true;
+        //dice[0].events.onInputDown.add(this.generateDice, this);
+        player2Dices.push(dice[0]);
 
-        var rand2 = Math.floor(Math.random() * 6);
-        dice = game.add.sprite(gameSceneOriginX+i*50, 0, 'dices');
-        dice.frame=rand;
-        //  Enables all kind of input actions on this image (click, etc)
-        dice.inputEnabled = true;
-        dice.events.onInputDown.add(this.generateDice, this);
-        dices.push(dice);
 
-        var rand3 = Math.floor(Math.random() * 6);
-        dice = game.add.sprite(gameSceneOriginX+i*50, 0, 'dices');
-        dice.frame=rand;
+        dice[1] = game.add.sprite(gameSceneOriginX+2*50, 0, 'dices-yellow');
+        dice[1].frame=rand;
         //  Enables all kind of input actions on this image (click, etc)
-        dice.inputEnabled = true;
-        dice.events.onInputDown.add(this.generateDice, this);
-        dices.push(dice);
+        //dice[1].inputEnabled = true;
+        //dice[1].events.onInputDown.add(this.generateDice, this);
+        player2Dices.push(dice[1]);
 
-        var rand4 = Math.floor(Math.random() * 6);
-        dice = game.add.sprite(gameSceneOriginX+i*50, 0, 'dices');
-        dice.frame=rand;
-        //  Enables all kind of input actions on this image (click, etc)
-        dice.inputEnabled = true;
-        dice.events.onInputDown.add(this.generateDice, this);
-        dices.push(dice);
 
-        var rand5 = Math.floor(Math.random() * 6);
-        dice = game.add.sprite(gameSceneOriginX+i*50, 0, 'dices');
-        dice.frame=rand;
+        dice[2] = game.add.sprite(gameSceneOriginX+4*50, 0, 'dices-gray');
+        dice[2].frame=rand;
         //  Enables all kind of input actions on this image (click, etc)
-        dice.inputEnabled = true;
-        dice.events.onInputDown.add(this.generateDice, this);
-        dices.push(dice);
+        //dice[2].inputEnabled = true;
+        //dice[2].events.onInputDown.add(this.generateDice, this);
+        player2Dices.push(dice[2]);
+
+
+        dice[3] = game.add.sprite(gameSceneOriginX+6*50, 0, 'dices-green');
+        dice[3].frame=rand;
+        //  Enables all kind of input actions on this image (click, etc)
+        //dice[3].inputEnabled = true;
+        //dice[3].events.onInputDown.add(this.generateDice, this);
+        player2Dices.push(dice[3]);
+
+
+        dice[4] = game.add.sprite(gameSceneOriginX+8*50, 0, 'dices-blue');
+        dice[4].frame=rand;
+        //  Enables all kind of input actions on this image (click, etc)
+        //dice[4].inputEnabled = true;
+        //dice[4].events.onInputDown.add(this.generateDice, this);
+        player2Dices.push(dice[4]);
 
         // itinialize player1's dices
-        for (var i=0; i<grille.cols; i++){
-            if(i%2==0){
-                //this.generateDice(i*50,550);
-                var rand = Math.floor(Math.random() * 6);
-                dice = game.add.sprite(gameSceneOriginX+i*50, 550, 'dices');
-                dice.frame=rand;
+        //_____________________________________________________________________
 
-                //  Enables all kind of input actions on this image (click, etc)
-                dice.inputEnabled = true;
+        //this.generateDice(i*50,550);
 
-                dice.events.onInputDown.add(this.generateDice, this);
-                dices.push(dice);
-            }
+        dice[5] = game.add.sprite(gameSceneOriginX, 550, 'dices-red');
+        dice[5].frame=rand;
+        //  Enables all kind of input actions on this image (click, etc)
+        //dice[5].inputEnabled = true;
+        //dice[5].events.onInputDown.add(this.generateDice, this);
+        player1Dices.push(dice[5]);
 
-        }
+
+        dice[6] = game.add.sprite(gameSceneOriginX+2*50, 550, 'dices-yellow');
+        dice[6].frame=rand;
+        //  Enables all kind of input actions on this image (click, etc)
+        //dice[6].inputEnabled = true;
+        //dice[6].events.onInputDown.add(this.generateDice, this);
+        player1Dices.push(dice[6]);
+
+
+        dice[7] = game.add.sprite(gameSceneOriginX+4*50, 550, 'dices-gray');
+        dice[7].frame=rand;
+        //  Enables all kind of input actions on this image (click, etc)
+        //dice[7].inputEnabled = true;
+        //dice[7].events.onInputDown.add(this.generateDice, this);
+        player1Dices.push(dice[7]);
+
+
+        dice[8] = game.add.sprite(gameSceneOriginX+6*50, 550, 'dices-green');
+        dice[8].frame=rand;
+        //  Enables all kind of input actions on this image (click, etc)
+        //dice[8].inputEnabled = true;
+        //dice[8].events.onInputDown.add(this.generateDice, this);
+        player1Dices.push(dice[8]);
+
+
+        dice[9] = game.add.sprite(gameSceneOriginX+8*50, 550, 'dices-blue');
+        dice[9].frame=rand;
+        //  Enables all kind of input actions on this image (click, etc)
+        //dice[9].inputEnabled = true;
+        //dice[9].events.onInputDown.add(this.generateDice, this);
+        player1Dices.push(dice[9]);
+
 
     },
 
@@ -275,13 +324,20 @@ var mainState = {
 
     },
 
+    throwPlayer1Dices: function(){
+        for (var i=0; i<player1Dices.length; i++){
+            this.generateDice(player1Dices[i]);
+        }
+    },
 
-    movePawn: function(pawn,step){
-        //direction 1 =left, 2=top-left, 3 = top, 4=top-right ....8= bottom-left
-        for (var j=0; j<8;j++){
-
+    throwPlayer2Dices: function(){
+        for (var i=0; i<player2Dices.length; i++){
+            this.generateDice(player2Dices[i]);
         }
     }
+
+
+
 
 
 };
